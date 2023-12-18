80d254adf2a36269160184d9aa867337a09ca66a
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2023-12-18 21:57:53 +0530

    Create test_circle.py

---
+++ class_based_tests/tests/test_circle.py
@@ -0,0 +1,19 @@
+import math
+
+import source.shapes as shapes
+
+
+class TestCircle:
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        self.circle = shapes.Circle(10)
+
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_perimeter(self):
+        assert self.circle.perimeter() == 2 * math.pi * self.circle.radius
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        del self.circle
